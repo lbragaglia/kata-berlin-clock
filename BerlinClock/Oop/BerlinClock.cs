053988e7986845940440816220ac13b95a8db09e
Lorenzo Bragaglia <lbragaglia@users.noreply.github.com>
2023-05-22 18:57:59 +0200

    Pre-init lamps

--- BerlinClock/Oop/BerlinClock.cs
+++ BerlinClock/Oop/BerlinClock.cs
@@ -1,13 +1,9 @@
-﻿namespace BerlinClock.Oop;
+﻿using static BerlinClock.Oop.BerlinClock.Lamp;
+
+namespace BerlinClock.Oop;
 
 public class BerlinClock
 {
-    private const int SecondsRowSize = 1;
-    private const int FiveHoursRowSize = 4;
-    private const int SingleHoursRowSize = 4;
-    private const int FiveMinutesRowSize = 11;
-    private const int SingleMinutesRowSize = 4;
-
     public enum Lamp
     {
         Yellow = 1,
@@ -15,42 +11,30 @@ public class BerlinClock
         Off = 0
     }
 
-    private readonly Lamp[] _secondsRow = new Lamp[SecondsRowSize];
-    private readonly Lamp[] _fiveHoursRow = new Lamp[FiveHoursRowSize];
-    private readonly Lamp[] _singleHoursRow = new Lamp[SingleHoursRowSize];
-    private readonly Lamp[] _fiveMinutesRow = new Lamp[FiveMinutesRowSize];
-    private readonly Lamp[] _singleMinutesRow = new Lamp[SingleMinutesRowSize];
+    private readonly Lamp[] _secondsRow = { Yellow };
+    private readonly Lamp[] _fiveHoursRow = { Red, Red, Red, Red };
+    private readonly Lamp[] _singleHoursRow = { Red, Red, Red, Red };
+
+    private readonly Lamp[] _fiveMinutesRow =
+        { Yellow, Yellow, Red, Yellow, Yellow, Red, Yellow, Yellow, Red, Yellow, Yellow };
+
+    private readonly Lamp[] _singleMinutesRow = { Yellow, Yellow, Yellow, Yellow };
 
     public BerlinClock(TimeSpan time)
     {
         var berlinTime = new BerlinTime(time);
-        _secondsRow[0] = berlinTime.IsSecondsLampSwitchedOn() ? Lamp.Yellow : Lamp.Off;
-        for (var i = 0; i < FiveHoursRowSize; i++)
-        {
-            _fiveHoursRow[i] = i < berlinTime.GetFiveHoursRowSwitchedOnLamps()
-                ? Lamp.Red
-                : Lamp.Off;
-        }
+        SwitchOff(_secondsRow, berlinTime.IsSecondsLampSwitchedOn() ? 1 : 0);
+        SwitchOff(_fiveHoursRow, berlinTime.GetFiveHoursRowSwitchedOnLamps());
+        SwitchOff(_singleHoursRow, berlinTime.GetSingleHoursRowSwitchedOnLamps());
+        SwitchOff(_fiveMinutesRow, berlinTime.GetFiveMinutesRowSwitchedOnLamps());
+        SwitchOff(_singleMinutesRow, berlinTime.GetSingleMinutesRowSwitchedOnLamps());
+    }
 
-        for (var i = 0; i < SingleHoursRowSize; i++)
+    private static void SwitchOff(IList<Lamp> lamps, int switchedOn)
+    {
+        for (var i = switchedOn; i < lamps.Count; i++)
         {
-            _singleHoursRow[i] = i < berlinTime.GetSingleHoursRowSwitchedOnLamps()
-                ? Lamp.Red
-                : Lamp.Off;
-        }
-
-        for (var i = 0; i < FiveMinutesRowSize; i++)
-        {
-            _fiveMinutesRow[i] = i < berlinTime.GetFiveMinutesRowSwitchedOnLamps()
-                ? (i + 1) % 3 == 0 ? Lamp.Red : Lamp.Yellow
-                : Lamp.Off;
-        }
-
-        for (var i = 0; i < SingleMinutesRowSize; i++)
-        {
-            _singleMinutesRow[i] = i < berlinTime.GetSingleMinutesRowSwitchedOnLamps()
-                ? Lamp.Yellow
-                : Lamp.Off;
+            lamps[i] = Off;
         }
     }
 
